c1b4e66a6d1778cef5d14b60e144109295954051
Andrew <andrewyin1994@gmail.com>
2019-07-29 15:46:59 -0700

    discovered async issue

---
+++ src/middleware/basic-auth.js
@@ -0,0 +1,16 @@
+function requireAuth(req, res, next) {
+  console.log('requireAuth');
+  console.log(req.get('Authorization'));
+
+  const authToken = req.get('Authorization') || '';
+
+  if (!authToken.toLowerCase().startsWith('basic ')) {
+    return res.status(401).json({ error: 'missing basic token' });
+  }
+
+  next();
+}
+
+module.exports = {
+  requireAuth
+};
